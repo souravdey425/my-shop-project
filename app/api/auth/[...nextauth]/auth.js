8a0ea046654f7b73abdb8d0db6fb7c865f813c36
Sourav Dey <souravdey425.sd@gmail.com>
2023-07-08 22:21:09 +0530

    /change

---
+++ app/api/auth/[...nextauth]/auth.js
@@ -0,0 +1,68 @@
+import prisma from '../../../lib/prismadb'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import GithubProvider from "next-auth/providers/github";
+import bcrypt from 'bcrypt'
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        // GithubProvider({
+        //     clientId: process.env.GITHUB_ID,
+        //     clientSecret: process.env.GITHUB_SECRET,
+        // }),
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_ID,
+        //     clientSecret: process.env.GOOGLE_SECRET,
+        // }),
+        CredentialsProvider({
+            name: "credentials",
+            // credentials: {
+            //     email: { label: "Email", type: "text", placeholder: "jsmith" },
+            //     password: { label: "Password", type: "text" },
+            //     username: { label: "Username", type: "text", placeholder: "John Smith" },
+            // },
+            async authorize(credentials) {
+              
+                // check to see if email and password is there
+                if(!credentials.email || !credentials.password) {
+                    throw new Error('Please enter an email and password')
+                }
+
+                // check to see if user exists
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                // if no user was found 
+                if (!user || !user?.hashedPassword) {
+                    throw new Error('No user found')
+                }
+
+                // check to see if password matches
+                const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+                // if password does not match
+                if (!passwordMatch) {
+                    throw new Error('Incorrect password')
+                }
+                if(!user){
+                    return null
+                }
+
+                return user;
+            },
+        }),  
+    ],
+    secret: process.env.SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    debug:process.env.NODE_ENV === "development",
+    pages:{
+        signIn:"/"
+    }
+}
